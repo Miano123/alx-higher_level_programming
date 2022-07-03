ce2ba133a4d4b2adf7c92d8e0f1857e6e0a2d575
Miano123 <rachelmiano91@gmail.com>
2022-07-03 21:48:08 +0300

    Print numbers separated by commas

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 for number in range(0, 99):
-    print("{:02d}".format(number), end=",")
+    if i < 99:
+        print("{:02d}".format(number) end=",")
+    else:
+        print("{:d}".format(number))
