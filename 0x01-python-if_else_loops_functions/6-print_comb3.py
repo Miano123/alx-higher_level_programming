f4eaf6509ff85a0bd63529f3b431db8c67dc9616
Miano123 <rachelmiano91@gmail.com>
2022-07-05 20:22:42 +0300

    All possible combination

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for number in range(0,90):
+for number in range(0, 90):
     if (number % 10 > number / 10):
         if(number != 89):
             print("{:02d}, ".format(number), end="")
