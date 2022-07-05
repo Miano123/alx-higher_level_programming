9758fafe96adc7b2b3cb02935d9c42a42f5a89ca
Miano123 <rachelmiano91@gmail.com>
2022-07-05 20:01:01 +0300

    All possible combination

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+for number in range(0,100):
+    first = number / 10
+    second = number % 10
+    if first < second and first != second and number != 89:
+        print("{:02d}, ".format(number), end='')
+        if number == 89:
+            print("{}".format(number))
