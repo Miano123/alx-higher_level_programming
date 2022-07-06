fbb56a1789421f5c9f1b3c7e3869b69d03b416f1
Miano123 <rachelmiano91@gmail.com>
2022-07-06 21:07:52 +0300

    Ascii alphabets

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 for i in range(ord('z'), ord('a') - 1, -1):
-    print("{:i}".format((i - (0rd('a') - ord('A'))) if i % 2 else i), end='')
+    print("{:i}".format((i - (ord('a') - ord('A'))) if i % 2 else i), end='')
