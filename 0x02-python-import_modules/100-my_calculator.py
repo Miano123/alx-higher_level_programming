9121cf5d229977b102968f4e297af5cd93a0d76b
Miano123 <rachelmiano91@gmail.com>
2022-07-09 16:04:36 +0300

    Build my own calculator!

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,18 +1,14 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from sys import argv
     from calculator_1 import add, sub, mul, div
-    
-    operators = {"+": add, "-": sub, "*": mul, "/": div}
-
-    if len(argv) != 4:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
-    elif operators.get(argv[2]) is not None:
-            a = int(argv[1])
-            b = int(argv[3])
-            func = operators[argv[2]](a, b)
-            print("{} {} {} = {}".format(argv[1], argv[2], argv[3], func))
-        else:
-            print("Unknown operator.Available operators: +, -, * and /")
-            exit(1)
+    import sys
+    if len(sys.argv) - 1 != 3:
+        print("usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+    ops = {"+": add, "-": sub, "*": mul, "/": div}
+    if sys.argv[2] not in list(ops.keys()):
+        print("unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+        a = int(sys.argv[1])
+        b = int(sys.argv[3])
+        print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
